643e1a9fb78628088afa86419fe9a73aeb9582f6
MollyKressler <kressler.molly@gmail.com>
2024-02-15 16:29:26 +0000

    Code afternoon 15/02/2024

--- figures+tables_edna.R
+++ figures+tables_edna.R
@@ -132,102 +132,121 @@ c <- read.csv('EDNA/data_edna/resultsANDcopies_perStandard_andNegControl_En.encr
 
 ## import species results (copies per replicate sample)
 
+	## For R server 
 	copies <- read_csv("data_downloads_notpublic/compiledspeciesqPCR_copiesresults_withmetadata_2023eDNACornishBlue.csv")
 	ntc <- read_csv("data_downloads_notpublic/resultsANDcopies_perStandard_andNegControl_En.encras_TESTS_1to7_NOV2023.csv", 
 	                col_types = cols(...1 = col_skip(), X = col_skip()))%>%
 	  mutate(Assay.Role = case_when(Assay.Role == 'Negative' ~ 'NTC',
 	                                Assay.Role == 'Positive' ~ 'Standard'
 	                                , .default = as.character(Assay.Role)))
+	## For local 
+		copies <- read_csv("EDNA/data_edna/compiledspeciesqPCR_copiesresults_withmetadata_2023eDNACornishBlue.csv")
+		tech.copies <- read_csv("EDNA/data_edna/compiledspeciesqPCR_copiesTECHNICALreps_withmetadata_2023eDNACornishBlue.csv")
+		ntc <- read_csv("EDNA/data_edna/resultsANDcopies_perStandard_andNegControl_En.encras_TESTS_1to7_NOV2023.csv", 
+		                col_types = cols(...1 = col_skip(), X = col_skip()))%>%
+		  mutate(Assay.Role = case_when(Assay.Role == 'Negative' ~ 'NTC',
+		                                Assay.Role == 'Positive' ~ 'Standard'
+		                                , .default = as.character(Assay.Role)))
 	
-	##########
-	## - Separate the Field controls from the copies results
-	##########
-	## use 'copies', replicateID will include 'WBC' 
-	
-	field.controls <- copies %>% filter(grepl('WBC', replicateID)) %>%
-	  mutate('Assay.Role' = 'Field.Control') # This is the field controls for the water bottles (and metaprobes from joint deployments)
-	field.controls 
-	
-	samples <- copies %>% filter(!grepl('WBC', replicateID)) %>%
-	  mutate('Assay.Role' = 'Field.Sample') # This is all field samples, water bottles and metaprobes, without the field controls 
-	
-	eng_sf <- st_as_sf(st_read('compiledspeciesqPCR_copiesresults_withmetadata_2023eDNACornishBlue.shp'),crs='WGS84')%>% # spatial and meta 
+##########
+## - Separate the Field controls from the copies results
+##########
+## use 'copies', replicateID will include 'WBC' 
+
+field.controls <- copies %>% filter(grepl('WBC', replicateID)) %>%
+  mutate('Assay.Role' = 'Field.Control') # This is the field controls for the water bottles (and metaprobes from joint deployments)
+field.controls 
+techreps.field.controls <- tech.copies %>% filter(grepl('WBC', replicateID)) %>%
+  mutate('Assay.Role' = 'Field.Control') 
+techreps.field.controls 
+
+samples <- copies %>% filter(!grepl('WBC', replicateID)) %>%
+  mutate('Assay.Role' = 'Field.Sample') # This is all field samples, water bottles and metaprobes, without the field controls 
+techreps.samples <- tech.copies %>% filter(!grepl('WBC', replicateID)) %>%
+  mutate('Assay.Role' = 'Field.Sample')
+
+samps_sf <- st_as_sf(st_read('EDNA/data_edna/compiledspeciesqPCR_copiesresults_withmetadata_2023eDNACornishBlue.shp'),crs='WGS84')%>%
 		rename(sampleID = samplID,
-			Sample.Name = Smpl_Nm,
-			replicateID = rplctID,
-			engraulis.cq.mean = engrl__,
-			engraulis.copies = engrls_,
-			sampDATE = smpDATE,
-			methodtype = mthdtyp)
-	
-	##########
-	## - Visualizing Amplification Results
-	##########
-	## stack datasets of field samples, field controls, and lab controls, & select down to only the columns we need - testID, Assay.Role, Sample.Name, species.cq.mean, species.copies
+		Sample.Name = Smpl_Nm,
+		replicateID = rplctID,
+		engraulis.cq.mean = engrl__,
+		mean.engraulis.copies = engrl__,
+		sampDATE = smpDATE,
+		methodtype = mthdtyp)
+techreps_sf <- st_as_sf(st_read('EDNA/data_edna/compiledspeciesqPCR_copiesTECHNICALreps_withmetadata_2023eDNACornishBlue.shp'),crs='WGS84')%>%
+		rename(sampleID = samplID,
+		Sample.Name = Smpl_Nm,
+		replicateID = rplctID,
+		engraulis.cq.mean = engrl__,
+		mean.engraulis.copies = engrl__,
+		sampDATE = smpDATE,
+		methodtype = mthdtyp)
+
+
+##########
+## - Visualizing Amplification Results
+##########
+## stack datasets of technical replicate field samples, field controls, and lab controls 
 	## engraulis 
-	eng.samples2 <- samples %>% dplyr::select(eventID,sampleID,testID, Assay.Role, Sample.Name, engraulis.cq.mean,engraulis.copies,methodtype)
-	eng.field.controls2 <- field.controls %>% 
-		dplyr::select(eventID,sampleID,testID, Assay.Role, Sample.Name, engraulis.cq.mean,engraulis.copies,methodtype)
-	eng.ntc2 <- ntc %>% 
-		dplyr::select(testID, Assay.Role, Sample.Name, Cq.Mean,engraulis.copies)%>%
-		rename(engraulis.cq.mean=Cq.Mean)%>%
-		mutate(sampleID = 'lab.controls',eventID = 'lab.controls',methodtype = 'lab.controls')
+		eng.samples2 <- techreps.samples %>% dplyr::select(eventID,sampleID,testID, Assay.Role, Sample.Name, engraulis.Cq,engraulis.copies,methodtype)%>%
+			mutate(Cq = case_when(is.na(engraulis.Cq) ~ 43, .default = as.numeric(engraulis.Cq)))
+		eng.field.controls2 <- techreps.field.controls %>% 
+			dplyr::select(eventID,sampleID,testID, Assay.Role, Sample.Name, engraulis.Cq,engraulis.copies,methodtype)%>%
+			mutate(engraulis.Cq = case_when(is.na(engraulis.Cq) ~ 43, .default = as.numeric(engraulis.Cq)))
+		eng.ntc2 <- ntc %>% 
+			dplyr::select(testID, Assay.Role, Sample.Name, engraulis.Cq,engraulis.copies)%>%
+			mutate(sampleID = 'lab.controls',eventID = 'lab.controls',methodtype = 'lab.controls')%>%
+			mutate(engraulis.Cq = case_when(is.na(engraulis.Cq) & Assay.Role == 'NTC' ~ 43, .default = as.numeric(engraulis.Cq)))## where Cq is NA (aka 0) matters but these wont show on a plot showing the amplification of samples and standards and NTCs - so we add in a false amplification one cycle LONGER than it ran for, to symbolise NA amplification on a plot. 
+
+		eng.f2 <- bind_rows(eng.samples2, eng.field.controls2, eng.ntc2)
+
+#### PLOT: Cq.value/mean by type (NTC, Standard, Field Sample, Field Control) - species specific
 	
-	eng.f2 <- bind_rows(eng.samples2, eng.field.controls2, eng.ntc2)
-	
-	## where Cq.Mean is zero matters but these wont show on a plot showing the amplification of samples and standards and NTCs - so we add in a false amplification one cycle LONGER than it ran for. 
-	
-	eng.ntcs.na <- eng.f2%>%
-	  filter(is.na(engraulis.cq.mean) & Assay.Role == 'NTC')%>%
-	  mutate(forplot = 43)
-	eng.field.sample.nas <- eng.f2%>%
-	  filter(is.na(engraulis.cq.mean) & Assay.Role == 'Field.Sample')%>%
-	  mutate(forplot = 43)
-	
-	#### PLOT: Cq.value/mean by type (NTC, Standard, Field Sample, Field Control) - species specific
-	## plot them as dots with colors based on Assay.Role
-	cqmeans_allwells_engraulis <- ggplot()+
-	       geom_point(data=eng.f2%>%filter(engraulis.cq.mean !='NA'), aes(x = as_factor(testID), y = engraulis.cq.mean, col = Assay.Role), size=3)+
-	      scale_color_manual(values = c('#DAA507','#8EC7D2','#733A4F','#07475A'))+
-	      geom_point(data=eng.ntcs.na, aes(x = as_factor(testID), y= forplot), size=5, pch=8, stroke = 1.5, col='#733A4F')+
-	    geom_point(data=eng.field.sample.nas, aes(x = as_factor(testID), y= forplot), size=2, pch=8, col='#8EC7D2', position = 'jitter')+
-	     ylim(0,50)+
-	      ylab('CQ (mean)')+
-	      xlab('Assay')+ 
-	      theme_bw()
-    
-		cqmeans_allwells_engraulis
-		ggsave(cqmeans_allwells_engraulis,file = 'EDNA/data_edna/figures_and_tables/cqmeans_allassays_engraulis_plot.png', device=png,units='in',height=5,width=6.5,dpi=700)
+	## engraulis	
+		cq_techreps_engraulis <- ggplot()+
+		       geom_point(data=eng.f2%>%filter(engraulis.Cq != 43), aes(x = as_factor(testID), y = engraulis.Cq, col = Assay.Role), size=3)+
+		      scale_color_manual(values = c('#DAA507','#8EC7D2','#733A4F','#07475A'))+
+		      geom_point(data=eng.f2%>%filter(engraulis.Cq == 43 & Assay.Role =='NTC'), aes(x = as_factor(testID), y= engraulis.Cq), size=5, pch=8, stroke = 1.5, col='#733A4F')+
+		    geom_point(data=eng.f2%>%filter(engraulis.Cq == 43 & Assay.Role =='Field.Control'), aes(x = as_factor(testID), y= engraulis.Cq), size=2, pch=8, col='#8EC7D2', position = 'jitter')+
+		     ylim(0,50)+
+		      ylab('CQ')+
+		      xlab('Assay')+ 
+		      theme_bw()
+		  
+			cq_techreps_engraulis
+
+			## save
+			ggsave(cq_techreps_engraulis,file = 'EDNA/data_edna/figures_and_tables/cqmeans_allassays_engraulis_plot.png', device=png,units='in',height=5,width=6.5,dpi=700)
 
 
 
-  ## Species standard curve test results
-		  ## engraulis
-	
+## Species standard curve test results
+	  ## engraulis
 
 
-  ## Glmer model of copy number between methods type, random effect of event ID
-    lm1 <- readRDS('data_edna/modelRDS/glmer_RandomEventID_allspecies_copies_byMethodType.RDS') ## all species
-    lm2 <- readRDS('data_edna/modelRDS/glmer_RandomEventID_fish_copies_byMethodType.RDS') ## fish 
-    lm3 <- readRDS('data_edna/modelRDS/glmer_RandomEventID_sharks_copies_byMethodType.RDS') ## sharks   
 
-    models <- c(lm1, lm2, lm3)
+## Glmer model of copy number between methods type, random effect of event ID
+  lm1 <- readRDS('data_edna/modelRDS/glmer_RandomEventID_allspecies_copies_byMethodType.RDS') ## all species
+  lm2 <- readRDS('data_edna/modelRDS/glmer_RandomEventID_fish_copies_byMethodType.RDS') ## fish 
+  lm3 <- readRDS('data_edna/modelRDS/glmer_RandomEventID_sharks_copies_byMethodType.RDS') ## sharks   
 
-    summary <- modelsummary(models, coef_rename = c('methodtypewaterbottle' = 'Water Bottle', 'methodmetaprobe' = 'Metaprobe'),fmt=3,estimate='estimate', statistic='conf.int',stars=FALSE,conf_level=0.95,output='flextable')
+  models <- c(lm1, lm2, lm3)
 
-    summary_flex <- summary %>%
-        set_header_labels('Model 1' = 'All Species', 'Model 2' = 'Fish Species', 'Model 3' = 'Shark Species')%>%
-        theme_zebra()%>%
-        align(align = 'center', part = 'all')%>%
-        font(fontname = 'Arial', part = 'all')%>%
-        fontsize(size = 10, part = 'all')%>%
-        autofit()
+  summary <- modelsummary(models, coef_rename = c('methodtypewaterbottle' = 'Water Bottle', 'methodmetaprobe' = 'Metaprobe'),fmt=3,estimate='estimate', statistic='conf.int',stars=FALSE,conf_level=0.95,output='flextable')
 
-    save_as_image(summary_flex, 'summary_glmers_eventIDrandom_copies_byMethod.png', webshot = 'webshot2')
+  summary_flex <- summary %>%
+      set_header_labels('Model 1' = 'All Species', 'Model 2' = 'Fish Species', 'Model 3' = 'Shark Species')%>%
+      theme_zebra()%>%
+      align(align = 'center', part = 'all')%>%
+      font(fontname = 'Arial', part = 'all')%>%
+      fontsize(size = 10, part = 'all')%>%
+      autofit()
 
-     
+  save_as_image(summary_flex, 'summary_glmers_eventIDrandom_copies_byMethod.png', webshot = 'webshot2')
 
-  ## Mega Table, grouped by Event ID: replicate copies and CQ results, corresponding experiment TestID, NTCs and Standards CQ values. 
+   
+
+## Mega Table, grouped by Event ID: replicate copies and CQ results, corresponding experiment TestID, NTCs and Standards CQ values. 
 
 
 
