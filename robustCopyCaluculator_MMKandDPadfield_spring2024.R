ef88bd1ec73d52ee8ecb4405e05d82129cff8e5a
MollyKressler <kressler.molly@gmail.com>
2024-05-28 12:22:10 +0100

    Code morning 28.05.2024
    New way (more robust) to calculate copy numbers from Cq values considering species specific LOD and LOQ. This will be integrated into the qPCR data pipeline. Much help from Dan Padfield @padpadpadpad on GitHub.

---
+++ robustCopyCaluculator_MMKandDPadfield_spring2024.R
@@ -0,0 +1,135 @@
+## Hello Daniel 
+
+library(tidyverse)
+
+## Little dataset
+rep <- c(10.2, 10.2,10.2,10.2,10.3,10.3,10.3,10.3,9.1,9.1,9.1,9.1)
+well <- c('A1','A2','A3','A4','B1','B2','B3','B4','C1','C2','C3','C4')
+Cq <- c(NA,NA,32,29,NA,15,16,14,NA,NA,NA,34)
+
+data <- cbind(rep, well, Cq)
+	## each sample (e.g. 10.2) has four technical replicates, identifiable by well-ID.
+	## the Cq is the cycle at which the well amplified.
+
+## Quantiative Standards
+LOD = 35
+LOQ = 29
+
+## Objective: when Cq == NA set the Cq equal to the LOD IF the Cqs of any other technical replicates are !=NA and ≤LOQ (less than because lower Cq is earlier amplification). Even one that meets the non-NA & ≤LOQ criteria is enough, like with tech reps for 10.2. 
+## Where technical replicates Cqs != NA and ≥LOQ, i.e. here any value above 29, ignore/don't mutate the Cq value.
+
+
+## thanks for the help.  
+
+# 1. convert data to dataframe
+data <- data.frame(data)
+head(data)
+str(data)
+
+# 2. When Cq == NA - set the Cq equal to the LOD if the CQs of any other technical replicates are !=NA and ≤LOQ
+
+# make a column to check whether any of the values are <LOQ
+# Give them a 1 and if not replace with NA
+data <- group_by(data, rep) %>%
+  mutate(., loq_check = ifelse(Cq <= LOQ & !is.na(Cq), 1, NA)) %>%
+  ungroup()
+
+# where a 1 exists replace all NAs with 1
+data <- group_by(data, rep) %>%
+  mutate(., loq_check = ifelse(any(loq_check == 1), 1, NA)) %>%
+  ungroup()
+
+# where loq check == 1 and Cq == NA replace with LOD
+data <- mutate(data, Cq = ifelse(loq_check == 1 & is.na(Cq), LOD, Cq))
+
+## Molly - 2-3 may 2024
+
+# 3. Where the sampling replicate is unreliable, i.e. it has NAs still after this (2) step, it needs to not be used in the averaging. 
+
+data2 <- group_by(data, rep) %>%
+  mutate(., reliable = ifelse(loq_check == is.numeric(loq_check), TRUE))%>%
+  mutate(reliable = replace_na(reliable,FALSE))%>%
+  mutate(Cq.adj = case_when(reliable == 'TRUE' ~ as.numeric(Cq), reliable == 'FALSE' ~ 0))%>%
+  ungroup()
+data2
+
+# 4. Next step would be calculating the copy numbers for each tech rep. 
+
+  calculate_copies <- function(intercept, slope, Cq.adj) {
+    copies <- 10^((Cq.adj - intercept)/slope)
+    return(copies)
+    }
+
+slope = -5
+intercept = 0.05
+
+data3 <- group_by(data2, rep) %>%
+ mutate(copies = if_else(reliable, calculate_copies(intercept, slope, Cq.adj), NA_real_))
+data3
+
+# 5. Calculate sampling replicate average copy number 
+
+data4 <- group_by(data3, rep) %>%
+  mutate(copies.avg = if_else(reliable, mean(copies), NA_real_))
+data4
+
+
+
+
+
+##############
+## - Make it pipe friendly
+##############
+## stes 1-5 above achieve the target: calculate copies for tech reps based on sequential conditions and their outcomes.
+  ## (a) that non-amplified tech reps are set to the LOD when the Cqs of any other tech reps (within he same sampling rep) have amplified and are less than or equalt to the LOQ. This is called the LOQ-check.
+  ## (b) determine if a tech rep is reliable based on the LOQ-check. Assign the Cq based on reliability: where TRUE, the Cq is not changed; where FALSE, the Cq is set to 0. 
+  ## (c) calculate copies based on the Cq.adj using the formula. 
+
+## define the calculate copies function before running pipe.
+  calculate_copies <- function(intercept, slope, Cq.adj) {
+    copies <- 10^((Cq.adj - intercept)/slope)
+    return(copies)
+    }
+    ## it would pull values from the NEB df but her we'll feed it values. 
+    slope = -5
+    intercept = 0.05
+
+
+input <- data.frame(data)%>%dplyr::select(-well)
+
+output <- group_by(input, rep) %>%
+  mutate(., loq_check = ifelse(Cq <= LOQ & !is.na(Cq), 1, NA)) %>%
+  ungroup()%>%
+  group_by(rep) %>%
+  mutate(., loq_check = ifelse(any(loq_check == 1), 1, NA)) %>%
+  ungroup()%>% mutate(Cq = ifelse(loq_check == 1 & is.na(Cq), LOD, Cq))%>%group_by(rep) %>%
+  mutate(., reliable = ifelse(loq_check == is.numeric(loq_check), TRUE))%>%
+  mutate(reliable = replace_na(reliable,FALSE))%>%
+  mutate(Cq.adj = case_when(reliable == 'TRUE' ~ as.numeric(Cq), reliable == 'FALSE' ~ 0))%>%
+  ungroup()%>%
+  group_by(rep) %>%
+  mutate(copies = if_else(reliable, calculate_copies(intercept, slope, Cq.adj), NA_real_))%>%
+  mutate(copies.avg = if_else(reliable, mean(copies), NA_real_))
+
+output # yep!
+
+# data pre-requisites
+  ## data be in a data frame
+  ## has the following columns: samp rep ID, well number (OR tech rep ID), and Cq.  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
